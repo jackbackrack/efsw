b6210c72194fbea801a3170cfa55e493e14c5058
Jonathan Bachrach <jackbackrack@gmail.com>
2018-04-19 12:15:54 -0700

    make jitx clone of efsw

---
+++ CMakeFiles/efsw.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jrb/efsw/src/efsw/Debug.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/Debug.cpp.o"
+  "/home/jrb/efsw/src/efsw/DirWatcherGeneric.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/DirWatcherGeneric.cpp.o"
+  "/home/jrb/efsw/src/efsw/DirectorySnapshot.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/DirectorySnapshot.cpp.o"
+  "/home/jrb/efsw/src/efsw/DirectorySnapshotDiff.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/DirectorySnapshotDiff.cpp.o"
+  "/home/jrb/efsw/src/efsw/FileInfo.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/FileInfo.cpp.o"
+  "/home/jrb/efsw/src/efsw/FileSystem.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/FileSystem.cpp.o"
+  "/home/jrb/efsw/src/efsw/FileWatcher.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/FileWatcher.cpp.o"
+  "/home/jrb/efsw/src/efsw/FileWatcherCWrapper.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/FileWatcherCWrapper.cpp.o"
+  "/home/jrb/efsw/src/efsw/FileWatcherGeneric.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/FileWatcherGeneric.cpp.o"
+  "/home/jrb/efsw/src/efsw/FileWatcherImpl.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/FileWatcherImpl.cpp.o"
+  "/home/jrb/efsw/src/efsw/FileWatcherInotify.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/FileWatcherInotify.cpp.o"
+  "/home/jrb/efsw/src/efsw/Log.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/Log.cpp.o"
+  "/home/jrb/efsw/src/efsw/Mutex.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/Mutex.cpp.o"
+  "/home/jrb/efsw/src/efsw/String.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/String.cpp.o"
+  "/home/jrb/efsw/src/efsw/System.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/System.cpp.o"
+  "/home/jrb/efsw/src/efsw/Thread.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/Thread.cpp.o"
+  "/home/jrb/efsw/src/efsw/Watcher.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/Watcher.cpp.o"
+  "/home/jrb/efsw/src/efsw/WatcherGeneric.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/WatcherGeneric.cpp.o"
+  "/home/jrb/efsw/src/efsw/WatcherInotify.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/WatcherInotify.cpp.o"
+  "/home/jrb/efsw/src/efsw/platform/posix/FileSystemImpl.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/platform/posix/FileSystemImpl.cpp.o"
+  "/home/jrb/efsw/src/efsw/platform/posix/MutexImpl.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/platform/posix/MutexImpl.cpp.o"
+  "/home/jrb/efsw/src/efsw/platform/posix/SystemImpl.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/platform/posix/SystemImpl.cpp.o"
+  "/home/jrb/efsw/src/efsw/platform/posix/ThreadImpl.cpp" "/home/jrb/efsw/CMakeFiles/efsw.dir/src/efsw/platform/posix/ThreadImpl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EFSW_INOTIFY_NOSYS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/efsw.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/efsw.dir/src/efsw/Debug.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/DirectorySnapshot.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/DirectorySnapshotDiff.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/DirWatcherGeneric.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/FileInfo.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/FileSystem.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/FileWatcher.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/FileWatcherCWrapper.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/FileWatcherGeneric.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/FileWatcherImpl.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/FileWatcherInotify.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/Log.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/Mutex.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/String.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/System.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/Thread.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/Watcher.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/WatcherGeneric.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/WatcherInotify.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/platform/posix/FileSystemImpl.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/platform/posix/MutexImpl.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/platform/posix/SystemImpl.cpp.o"
+  "CMakeFiles/efsw.dir/src/efsw/platform/posix/ThreadImpl.cpp.o"
+  "libefsw.pdb"
+  "libefsw.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/efsw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
